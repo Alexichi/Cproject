68411a945e719fde99a0950e99b3d37065dadf05
Alexichi <alexrenoux96@gmail.com>
2016-12-23 14:19:23 +0100

    Implémentation des fonctions permettant de rentrer dans un tableau de polynome les polynômes écrits sur l'ES. Attention il reste des erreurs non corrigés. Ajout de commentaires.

--- polynome.c
+++ polynome.c
@@ -1,3 +1,7 @@
+/* Le degré du polynôme sera l'indice du tableau et la valeur 
+ * à cet indice sera le coefficient devant la variable x
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <regex.h>
@@ -5,16 +9,19 @@
 #define DEGREMAX 30
 typedef int polynome[DEGREMAX];
 
-void isPolynomeCorrect(void);
-void triTableau(char *);
+int isPolynomeCorrect(char * );
+void triTableau(char *, char *);
+void tabToPol(char * , polynome , int );
+int isdigit (int );
 
 int main(void){
 	int c;
 	int i = 0;
 	int j = 0;
 	char tousLesC[200]; //tableau contenant tous les caractères entrées sur l'ES
-	char tableauTrie[2][DEGREMAX];
-	int polynomes[10][2][DEGREMAX]; /*tableau contenant tous les polynômes (10 max).Le 2 correspond à un 
+	char tableauTrie[DEGREMAX];
+	polynome p;
+	polynome polynomes[10][DEGREMAX]; /*tableau contenant tous les polynômes (10 max).Le 2 correspond à un 
 									 * tableau comprenant les coefficients et les puissances. 
 									 */
 	printf("Entrez un polynôme :\n");
@@ -22,7 +29,9 @@ int main(void){
 		if((c=getchar()) == '\n'){
 			if(isPolynomeCorrect(tousLesC)){
 				triTableau(tousLesC, tableauTrie);
-				polynomes[i] = tableauTrie;
+				tabToPol(tableauTrie, p, DEGREMAX);
+				polynomes[i] = p;
+				i++;
 			}
 			else{
 				printf ("ce n\'est pas un polynome valide\n");
@@ -44,16 +53,42 @@ int main(void){
  * en récupérant seulement les chiffres en faisant attention à différencier les 
  * coefficients des puissances.
  */
-void triTableau(char * tabATrier, char * tabTrier[DEGREMAX]){
+void triTableau(char * tabATrier, char * pol){
+	int j = 0;
 	for(int i = 0; i < 200; i++){
-		
+		if(tabATrier[i] == 'x' && tabATrier[i-1] == '*'){ //le polynome est de la forme a*x^b
+			j = i-2;
+		}
+		else if(tabATrier[i] == 'x' && isdigit(tabATrier[i-1])){ //le polynome est de la forme ax^b
+			j = i-1;
+		}
+		while(isdigit(tabATrier[j])){
+				pol[0][0] =  pol[0][0] + tabATrier[j];
+		}
 	}
 }
 
-void isPolynomeCorrect(char * request){
+/* Fonction qui converti le tableau trié contenant les coefficients et 
+ * les puissances en polynome.
+ * tab[0] correspond aux coefficients et tab[1] correspond aux puissances
+ */
+void tabToPol(char * tab, polynome p, int size){
+	int power = 0;
+	for(int i ; i < size; i++){
+		power = tab[1][i]; //power prend la puissance à l'indice i du tableau des puissances
+		if(p[power] == 0){
+			p[power] = tab[0][i]; // le coefficient va à l'indice correspondant à sa puissance associée
+		}
+		else{
+			p[power] = p[power] + (int)tab[0][i]; //sinon on somme à la valeur déjà présente à cette indice
+		}
+	}
+}
+
+int isPolynomeCorrect(char * request){
 	int err;
 	regex_t preg;
-	const char *str_request = (string)request;
+	const char *str_request = request;
 	const char *str_regex = "[\s*[0-9]\*?*x[\\^[0-9]+]?\s*\+|\-\s*]+";
 	
 	err = regcomp (&preg, str_regex, REG_NOSUB | REG_EXTENDED);
@@ -65,10 +100,12 @@ void isPolynomeCorrect(char * request){
 		regfree (&preg);
 		
 		if (match == 0){
-			printf ("%s est un polynome valide\n", str_request);
+			//printf ("%s est un polynome valide\n", str_request);
+			return 1;
 		}
 		else if (match == REG_NOMATCH){
-			printf ("%s n\'est pas un polynome valide\n", str_request);
+			//printf ("%s n\'est pas un polynome valide\n", str_request);
+			return 0;
 		}
 		else{
 			char *text;
@@ -86,7 +123,9 @@ void isPolynomeCorrect(char * request){
 			}
 		}
 	}
-	puts ("\nPress any key\n");
-	/* Dev-cpp */
-	getchar ();
 }
+
+/* puts ("\nPress any key\n");
+	//Dev-cpp
+	getchar ();
+	*/
